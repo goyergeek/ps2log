ce36d607ec3fb75d3a83d37311e1e402e495990d
goyergeek <goyergeek@protonmail.com>
2024-05-13 17:53:12 -0600

    Removed old functions from module, refactored new functions.

--- ps2log.psm1
+++ ps2log.psm1
@@ -24,7 +24,7 @@ function dtStamp {
     } else {
         return $LogDate
     }
-} # End Of Function
+} # End Of dtStamp Function
 
 
 ### End Of Private Functions. ###
@@ -39,6 +39,7 @@ function dtStamp {
 #>
 
 # New-ps2logConfig
+# Creates a new ps2log configuration file at the specified path.
 function New-ps2LogConfig {
     [CmdletBinding(SupportsShouldProcess)]
     param (
@@ -47,27 +48,27 @@ function New-ps2LogConfig {
         [string] $path,
 
         [Parameter(Mandatory=$False,ValueFromPipeLine=$True,Position=1)]
-        [ValidateScript({Test-Path $_})]
+        [ValidateScript({if(!(Test-Path $_)){New-Item $_ -ItemType Directory}Test-Path $_})]
         [string] $Logpath= "C:\Temp",
 
         [Parameter(Mandatory=$False,ValueFromPipeLine=$True,Position=2)]
-        [ValidateScript({Test-Path $_})]
+        [ValidateRange({10..100})]
         [int] $MaxFiles = 10,
 
         [Parameter(Mandatory=$False,ValueFromPipeLine=$True,Position=3)]
-        [ValidateScript({Test-Path $_})]
+        [ValidateRange({10..50})]
         [int] $MaxFileSizeMB = 20,
 
         [Parameter(Mandatory=$False,ValueFromPipeLine=$True,Position=4)]
-        [ValidateScript({Test-Path $_})]
+        [ValidateSet({$true,$false})]
         [bool] $ArchiveLogs = $True,
 
         [Parameter(Mandatory=$False,ValueFromPipeLine=$True,Position=5)]
-        [ValidateScript({Test-Path $_})]
+        [ValidateRange({10..100})]
         [int] $MaxArchiveFiles = 10,
 
         [Parameter(Mandatory=$False,ValueFromPipeLine=$True,Position=6)]
-        [ValidateScript({Test-Path $_})]
+        [ValidateSet({1,2,3})]
         [int] $LogLevel = 1  ## 1 = INFO, 2 = WARN, 3 = DEBUG (All logs contain error stacks)
     )
 
@@ -84,18 +85,106 @@ function New-ps2LogConfig {
             ArchiveLogs = [bool]$ArchiveLogs
             MaxArchiveFiles = [int]$MaxArchiveFiles
             LogLevel = [int]$LogLevel
+            ConfigDate = [string] $(dtStamp 1)
         }
     }
 
     Process {
-        $configObj | Out-File "$path\test.txt" -Force
+        $configObj | Out-File "$path\ps2log.config" -Force
     }
 
     End {
-        return $configObj
+        Write-Output $configObj
     }
     
-} # End Of Function
+} # End Of New-ps2logConfig Function
+
+# Set-ps2logConfig
+# Set one or more attributes for a ps2log configuration file at the specified path.
+function Set-ps2LogConfig {
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [Parameter(Mandatory=$True,ValueFromPipeLine=$True,Position=0)]
+        [ValidateScript({Test-Path $_})]
+        [string] $path,
+
+        [Parameter(Mandatory=$False,ValueFromPipeLine=$True,Position=1)]
+        [ValidateScript({if(!(Test-Path $_)){New-Item $_ -ItemType Directory}Test-Path $_})]
+        [string] $Logpath= "C:\Temp",
+
+        [Parameter(Mandatory=$False,ValueFromPipeLine=$True,Position=2)]
+        [ValidateRange({10..100})]
+        [int] $MaxFiles = 10,
+
+        [Parameter(Mandatory=$False,ValueFromPipeLine=$True,Position=3)]
+        [ValidateRange({10..50})]
+        [int] $MaxFileSizeMB = 20,
+
+        [Parameter(Mandatory=$False,ValueFromPipeLine=$True,Position=4)]
+        [ValidateSet({$true,$false})]
+        [bool] $ArchiveLogs = $True,
+
+        [Parameter(Mandatory=$False,ValueFromPipeLine=$True,Position=5)]
+        [ValidateRange({10..100})]
+        [int] $MaxArchiveFiles = 10,
+
+        [Parameter(Mandatory=$False,ValueFromPipeLine=$True,Position=6)]
+        [ValidateSet({1,2,3})]
+        [int] $LogLevel = 1  ## 1 = INFO, 2 = WARN, 3 = DEBUG (All logs contain error stacks)
+    )
+
+    # Begin: Process Parameters into Object
+    Begin {
+        if(!($path)){
+            exit(1)
+        }
+        $configObj = [PSCustomObject]@{
+            Path = "$path\ps2log.config"
+            LogPath = [string]$LogPath
+            MaxFiles = [int]$MaxFiles
+            MaxFileSizeMB = [int]$MaxFileSizeMB
+            ArchiveLogs = [bool]$ArchiveLogs
+            MaxArchiveFiles = [int]$MaxArchiveFiles
+            LogLevel = [int]$LogLevel
+            ConfigDate = [string] $(dtStamp 1)
+        }
+    }
+
+    Process {
+        $configObj | Out-File "$path\ps2log.config" -Force
+    }
+
+    End {
+        Write-Output $configObj
+    }
+    
+} # End Of New-ps2logConfig Function
+
+# Get-ps2logConfig
+# Fetches the ps2log configuration file from the specified location
+fucntion Get-ps2logConfig {
+    [CmdletBinding(SupportsShouldProcess)]
+    Param (
+        [Parameter(Mandatory=$true,ValueFromPipeLine=$true,Position=0)]
+        [ValidateScript({Test-Path $_})]
+        [string] $path
+    )
+    
+    begin {
+        if(!($path)){
+            exit(1)
+        }
+    }
+
+    process {
+        $ps2logConfig = Get-Content "$path\ps2logConfig.txt"
+    }
+
+    end {
+        Write-Output $ps2logConfig
+    }
+
+} # End Of Get-ps2logConfig Function
 
 # Write-PS2Log - Takes Log Object, Configuration File, and Writes logs to relavent channels based on content.
 function Write-PS2Log {
@@ -118,45 +207,3 @@ function Write-PS2Log {
 
     return [PSCustomObject] $logObj
 } # End Of Function
-
-
-# New-PS2LogObj - Takes input parameters and returns PSCustobObject with them embedded.
-function New-PS2LogObj {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory=$True,ValueFromPipeline=$True,Position=0)]
-        [ValidateNotNullOrEmptyAttribute()]
-        [INT] $lType,
-
-        [Parameter(Mandatory=$True,ValueFromPipeline=$True,Position=1)]
-        [ValidateNotNullOrEmptyAttribute()]
-        [string] $callingScript,
-
-        [Parameter(Mandatory=$True,ValueFromPipeline=$True,Position=2)]
-        [ValidateNotNullOrEmptyAttribute()]
-        [string] $msg,
-
-        [Parameter(Mandatory=$True,ValueFromPipeline=$True,Position=3)]
-        [ValidateNotNullOrEmptyAttribute()]
-        [ArrayList] $errArray,
-
-        [Parameter(Mandatory=$False,ValueFromPipeline=$True,Position=4)]
-        [ValidateNotNullOrEmptyAttribute()]
-        [xml] $customConfig
-    )
-
-    
-
-    $logObj = [PSCustomObject]@{
-        logType = $lType
-        callingScript = $callingScript
-        msg = $msg
-        $err = $errArray
-    }
-
-    return $logObj
-} # End Of Function
-
-
-
-
