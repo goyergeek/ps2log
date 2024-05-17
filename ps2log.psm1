7e83b7c021a7c6661c3a52ab28be033c76c16290
goyergeek <goyergeek@gmail.com>
2024-05-17 17:12:16 -0600

    Implemented ps2logConfig Class, started adding methods.

--- ps2log.psm1
+++ ps2log.psm1
@@ -1,21 +1,184 @@
-##################### Private Functions, Not Exported. #####################
+##################### Private Functions and Classes, Not Exported. #####################
+
+class ps2logConfig {
+    static [string] $Name = "Default"
+    static [string] $ConfigPath = "$env:APPDATA\ps2log"
+    static [string] $LogPath = "$env:APPDATA\ps2log\Logs"
+    static [Int] $MaxFiles = 10
+    static [Int] $MaxFileSizeMB = 10
+    static [bool] $ArchiveLogs = $false
+    static [Int] $MaxArchiveFiles = 100
+    static [Int] $LogLevel = 1     ### 1 = INFO, 2 = WARN, 3 = DEBUG (All logs contain error stacks)
+    hidden [string] $ConfigVersion = "1.05172024b"
+    hidden [guid] $ConfigID = [guid]::NewGuid()
+    hidden [datetime] $Created = $(Get-Date)
+    hidden [datetime] $Modified = $(Get-Date)
+
+    ps2logConfig(
+        [string] $Name,
+        [string] $ConfigPath,
+        [string] $LogPath,
+        [Int] $MaxFiles,
+        [Int] $MaxFileSizeMB,
+        [bool] $ArchiveLogs,
+        [Int] $MaxArchiveFiles,
+        [Int] $LogLevel
+        ) {
+        $this.Name = $Name
+        $this.ConfigPath = $ConfigPath
+        $this.LogPath = $LogPath
+        $this.MaxFiles = $MaxFiles
+        $this.MaxFileSizeMB = $MaxFileSizeMB
+        $this.ArchiveLogs = $ArchiveLogs
+        $this.MaxArchiveFiles = $MaxArchiveFiles
+        $this.LogLevel = $LogLevel
+    }
+
+    ps2logConfig(
+        [string] $ConfigPath,
+        [string] $LogPath,
+        [Int] $MaxFiles,
+        [Int] $MaxFileSizeMB,
+        [bool] $ArchiveLogs,
+        [Int] $MaxArchiveFiles,
+        [Int] $LogLevel
+        ) {
+        $this.ConfigPath = $ConfigPath
+        $this.LogPath = $LogPath
+        $this.MaxFiles = $MaxFiles
+        $this.MaxFileSizeMB = $MaxFileSizeMB
+        $this.ArchiveLogs = $ArchiveLogs
+        $this.MaxArchiveFiles = $MaxArchiveFiles
+        $this.LogLevel = $LogLevel
+    }
+
+    ps2logConfig(
+        [string] $LogPath,
+        [Int] $MaxFiles,
+        [Int] $MaxFileSizeMB,
+        [bool] $ArchiveLogs,
+        [Int] $MaxArchiveFiles,
+        [Int] $LogLevel
+        ) {
+        $this.LogPath = $LogPath
+        $this.MaxFiles = $MaxFiles
+        $this.MaxFileSizeMB = $MaxFileSizeMB
+        $this.ArchiveLogs = $ArchiveLogs
+        $this.MaxArchiveFiles = $MaxArchiveFiles
+        $this.LogLevel = $LogLevel
+    }
+
+    ps2logConfig(
+        [Int] $MaxFiles,
+        [Int] $MaxFileSizeMB,
+        [bool] $ArchiveLogs,
+        [Int] $MaxArchiveFiles,
+        [Int] $LogLevel
+        ) {
+        $this.MaxFiles = $MaxFiles
+        $this.MaxFileSizeMB = $MaxFileSizeMB
+        $this.ArchiveLogs = $ArchiveLogs
+        $this.MaxArchiveFiles = $MaxArchiveFiles
+        $this.LogLevel = $LogLevel
+    }
+
+    ps2logConfig(
+        [Int] $MaxFileSizeMB,
+        [bool] $ArchiveLogs,
+        [Int] $MaxArchiveFiles,
+        [Int] $LogLevel
+        ) {
+        $this.MaxFileSizeMB = $MaxFileSizeMB
+        $this.ArchiveLogs = $ArchiveLogs
+        $this.MaxArchiveFiles = $MaxArchiveFiles
+        $this.LogLevel = $LogLevel
+    }
+
+    ps2logConfig([bool] $ArchiveLogs, [Int] $MaxArchiveFiles, [Int] $LogLevel) {
+        $this.ArchiveLogs = $ArchiveLogs
+        $this.MaxArchiveFiles = $MaxArchiveFiles
+        $this.LogLevel = $LogLevel
+    }
+
+    ps2logConfig([Int] $MaxArchiveFiles, [Int] $LogLevel) {
+        $this.MaxArchiveFiles = $MaxArchiveFiles
+        $this.LogLevel = $LogLevel
+    }
+
+    ps2logConfig([Int] $LogLevel) {
+        $this.LogLevel = $LogLevel
+    }
+
+    ps2logConfig() {
+    
+    }
+
+    
+    ps2logConfig([hashtable]$info){
+        switch ($info.Keys){
+            'Name'              {$this.name = $info.Name}
+            'ConfigPath '       {$this.ConfigPath = $info.ConfigPath}
+            'LogPath'           {$this.LogPath = $info.LogPath}
+            'MaxFiles'          {$this.MaxFiles = $info.MaxFiles}
+            'MaxFileSizeMB'     {$this.MaxFileSizeMB = $info.MaxFileSizeMB}
+            'ArchiveLogs'       {$this.ArchiveLogs = $info.ArchiveLogs}
+            'MaxArchiveFiles'   {$this.MaxArchiveFiles = $info.MaxArchiveFiles}
+            'LogLevel'          {$this.LogLevel = $info.LogLevel}
+        }
+    }
+    
+    [void] Write([string]$type) {
+
+        if(!(Test-Path "$($this.ConfigPath)")){
+            New-Item -Path "$($this.ConfigPath)" -ItemType Directory -Force
+        }
+        
+        switch($type.ToLower()) {
+            'xml'{
+                [xml]$configData = @"
+<?xml version="1.0" encoding="utf-8"?>
+<ps2logConfigs type=`"ps2log`" version=`"$($this.ConfigVersion)`">
+`t<Config ID=`"$($this.ConfigID)`" Created=`"$($this.Created)`" Modified=`"$($this.Modified)`">
+`t`t<ConfigPath>$($this.ConfigPath)</ConfigPath>
+`t`t<LogPath>$($this.LogPath)</LogPath>
+`t`t<MaxFiles>$($this.MaxFiles)</MaxFiles>
+`t`t<MaxFileSizeMB>$($this.MaxFileSizeMB)</MaxFileSizeMB>
+`t`t<ArchiveLogs>$($this.ArchiveLogs)</ArchiveLogs>
+`t`t<MaxArchiveFiles>$($this.MaxArchiveFiles)</MaxArchiveFiles>
+`t`t<LogLevel>$($this.LogLevel)</LogLevel>
+`t</Config>
+</ps2logConfigs>
+"@
+                [xml]$configData.Save("$($this.ConfigPath)\ps2logConfig.xml")
+            }
+            'csv' {Write-Host "This should write a CSV File"}
+            'tab' {Write-Host "This should write a TSV file"}
+        }
+            
+    }
+
+    [void] Append([ps2logConfig]$configObj){
+        
+    }
+
+} # End Of Class ps2logConfig
+
 
 # dtStamp - helper function that does the obvious.
 function dtStamp {
     param (
-        [Parameter(Mandatory=$True,ValueFromPipeLine=$True,Position=0)]
+        [Parameter(Mandatory=$false,ValueFromPipeLine=$True,Position=0)]
         [ValidateSet(1,2)]
         [Int] $stampType
     )
-    [datetime] $FileDate = Get-Date -Format "MMDDYYYY-HHmmss"
-    [datetime] $LogDate = Get-Date -Format "MM\dd\yyyy HH:mm:ss:ffff"
+    $FileDate = Get-Date -Format "MMddyyyyHHmmss"
+    $LogDate = Get-Date -Format "MM/dd/yyyy HH:mm:ss:ffff"
     if ($stampType -eq 1) {
         return $FileDate
     } else {
         return $LogDate
     }
 } # End Of dtStamp Function
-
 
 ##################### End Of Private Functions. #####################
 
@@ -36,55 +199,61 @@ function New-ps2LogConfig {
     [CmdletBinding(SupportsShouldProcess)]
     param (
         [Parameter(Mandatory=$false,ValueFromPipeLine=$True,Position=0)]
-        [string] $path= "$($env:HOMEPATH)\ps2log\config",
+        [string] $Name= "Default",
 
-        [Parameter(Mandatory=$False,ValueFromPipeLine=$True,Position=1)]
-        [string] $Logpath= "$($env:HOMEPATH)\ps2log\logs",
+        [Parameter(Mandatory=$false,ValueFromPipeLine=$True,Position=1)]
+        [string] $path= "C:$($env:HOMEPATH)\ps2log\config",
 
         [Parameter(Mandatory=$False,ValueFromPipeLine=$True,Position=2)]
-        [ValidateRange({10..100})]
-        [int] $MaxFiles = 10,
+        [string] $Logpath= "C:$($env:HOMEPATH)\ps2log\logs",
 
         [Parameter(Mandatory=$False,ValueFromPipeLine=$True,Position=3)]
-        [ValidateRange({10..50})]
-        [int] $MaxFileSizeMB = 20,
+        [ValidateRange(10,100)]
+        [int] $MaxFiles = 10,
 
         [Parameter(Mandatory=$False,ValueFromPipeLine=$True,Position=4)]
+        [ValidateRange(10,50)]
+        [int] $MaxFileSizeMB = 20,
+
+        [Parameter(Mandatory=$False,ValueFromPipeLine=$True,Position=5)]
         [ValidateSet({$true,$false})]
         [bool] $ArchiveLogs = $True,
 
-        [Parameter(Mandatory=$False,ValueFromPipeLine=$True,Position=5)]
-        [ValidateRange({10..100})]
+        [Parameter(Mandatory=$False,ValueFromPipeLine=$True,Position=6)]
+        [ValidateRange(10,100)]
         [int] $MaxArchiveFiles = 10,
 
-        [Parameter(Mandatory=$False,ValueFromPipeLine=$True,Position=6)]
-        [ValidateSet({1,2,3})]
+        [Parameter(Mandatory=$False,ValueFromPipeLine=$True,Position=7)]
+        [ValidateSet(1,2,3)]
         [int] $LogLevel = 1  ## 1 = INFO, 2 = WARN, 3 = DEBUG (All logs contain error stacks)
     )
 
     # Begin: Process Parameters into Object
     Begin {
-        if(!(Test-Path $path)){
-            New-Item $path -ItemType Directory -Force
-        }
-    }
-
-    Process {
+        
         $configObj = [PSCustomObject]@{
-            Path = "$path\ps2log.config"
+            Name = [string] "Default"
+            CreatedDate = [string] $(dtStamp)
+            LastModified = [string] $(dtStamp)
+            Path = "$path\ps2logConf.xml"
             LogPath = [string]$LogPath
             MaxFiles = [int]$MaxFiles
             MaxFileSizeMB = [int]$MaxFileSizeMB
             ArchiveLogs = [bool]$ArchiveLogs
             MaxArchiveFiles = [int]$MaxArchiveFiles
             LogLevel = [int]$LogLevel
-            ConfigDate = [string] $(dtStamp 1)
         }
+    }
 
-        $configObj | Out-File "$path\ps2log.config" -Force
+    Process {
+        
+        if(!(Test-Path $path)){
+            New-Item $path -ItemType Directory -Force | Out-Null
+        }
     }
 
     End {
+
         Write-Output $configObj
     }
     
@@ -102,11 +271,11 @@ function Set-ps2LogConfig {
         [string] $Logpath,
 
         [Parameter(Mandatory=$False,ValueFromPipeLine=$True,Position=2)]
-        [ValidateRange({10..100})]
+        [ValidateRange({10,100})]
         [int] $MaxFiles,
 
         [Parameter(Mandatory=$False,ValueFromPipeLine=$True,Position=3)]
-        [ValidateRange({10..50})]
+        [ValidateRange({10,50})]
         [int] $MaxFileSizeMB,
 
         [Parameter(Mandatory=$False,ValueFromPipeLine=$True,Position=4)]
@@ -114,7 +283,7 @@ function Set-ps2LogConfig {
         [bool] $ArchiveLogs,
 
         [Parameter(Mandatory=$False,ValueFromPipeLine=$True,Position=5)]
-        [ValidateRange({10..100})]
+        [ValidateRange({10,100})]
         [int] $MaxArchiveFiles,
 
         [Parameter(Mandatory=$False,ValueFromPipeLine=$True,Position=6)]
